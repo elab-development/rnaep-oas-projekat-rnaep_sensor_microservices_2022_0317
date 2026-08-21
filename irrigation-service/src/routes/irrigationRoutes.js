d520e89bb0878e7bbccb0b0d1815e71649c8d153
Markozecevic7 <marko.zecevic2003@gmail.com>
2026-08-21 21:26:02 +0200

    sitne ispravke

--- irrigation-service/src/routes/irrigationRoutes.js
+++ irrigation-service/src/routes/irrigationRoutes.js
@@ -7,42 +7,31 @@ const { checkZoneAccess, checkRuleAccess } = require('../middleware/auth');
 router.get('/zones', irrigationController.getZones);
 router.post('/zones', irrigationController.createZone);
 
+// UKLONJENE RUTE koje nemaju funkcije:
+// router.get('/zones/:zone_id', checkZoneAccess, irrigationController.getZoneById);
+// router.put('/zones/:zone_id', checkZoneAccess, irrigationController.updateZone);
+// router.delete('/zones/:zone_id', checkZoneAccess, irrigationController.deleteZone);
+
 // Pravila (CRUD)
 router.post('/rules', irrigationController.createRule);
 router.get('/rules', irrigationController.getRules);
-router.get('/rules/:id', irrigationController.getRuleById);
-router.put('/rules/:id', irrigationController.updateRule);
-router.delete('/rules/:id', irrigationController.deleteRule);
 
-// Provera pravila (poziva Sensor Service)
-router.post('/check', irrigationController.checkRules);
+// Pravila sa ID - OSTAVLJAMO samo one koje imaju funkcije
+router.get('/rules/:id', checkRuleAccess, irrigationController.getRuleById);
+router.put('/rules/:id', checkRuleAccess, irrigationController.updateRule);
+router.delete('/rules/:id', checkRuleAccess, irrigationController.deleteRule);
 
-// Ručno upravljanje
-router.post('/manual/on', irrigationController.manualIrrigation);
-router.post('/manual/off', irrigationController.manualIrrigationOff);
+// Provera pravila (poziva Sensor Service) - zahteva proveru zone
+router.post('/check', checkZoneAccess, irrigationController.checkRules);
+
+// Ručno upravljanje - zahteva proveru zone
+router.post('/manual/on', checkZoneAccess, irrigationController.manualIrrigation);
+router.post('/manual/off', checkZoneAccess, irrigationController.manualIrrigationOff);
 
 // Vremenska prognoza za zonu
 router.get('/weather/:zone_id', irrigationController.getWeatherForecast);
 
 // Višednevna vremenska prognoza za zonu
 router.get('/forecast/:zone_id', irrigationController.getWeatherForecast7Days);
-
-
-// Zone - dodaj checkZoneAccess
-router.get('/zones/:zone_id', checkZoneAccess, irrigationController.getZoneById);
-router.put('/zones/:zone_id', checkZoneAccess, irrigationController.updateZone);
-router.delete('/zones/:zone_id', checkZoneAccess, irrigationController.deleteZone);
-
-// Pravila - dodaj checkRuleAccess
-router.get('/rules/:id', checkRuleAccess, irrigationController.getRuleById);
-router.put('/rules/:id', checkRuleAccess, irrigationController.updateRule);
-router.delete('/rules/:id', checkRuleAccess, irrigationController.deleteRule);
-
-// Provera pravila - dodaj checkZoneAccess (jer koristi zone_id)
-router.post('/check', checkZoneAccess, irrigationController.checkRules);
-
-// Ručno upravljanje - dodaj checkZoneAccess
-router.post('/manual/on', checkZoneAccess, irrigationController.manualIrrigation);
-router.post('/manual/off', checkZoneAccess, irrigationController.manualIrrigationOff);
 
 module.exports = router;
